c204700ccc6b2690626bef24bd33140482c0ac11
scottj13atwit <scottj13@wit.edu>
2025-03-29 20:31:53 -0400

    Add files via upload

---
+++ MedicalSoftware/src/MedicalSoftware/Patient.java
@@ -0,0 +1,5 @@
+package MedicalSoftware;
+
+public class Patient {
+
+}
---
+++ MedicalSoftware/src/module-info.java
@@ -0,0 +1,11 @@
+module MedicalSoftware {
+	exports MedicalSoftware;
+	
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.media;
+	requires javafx.swing;
+	requires javafx.web;
+}
