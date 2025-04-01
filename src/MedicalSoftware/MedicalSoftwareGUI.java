753e169f92205d1aebe2480c363fc6541e83e1f6
joneso2 <owenjones585@gmail.com>
2025-03-31 21:03:18 -0400

    TEST

--- src/MedicalSoftware/MedicalSoftwareGUI.java
+++ src/MedicalSoftware/MedicalSoftwareGUI.java
@@ -10,7 +10,7 @@ public class MedicalSoftwareGUI extends Application {
 	@Override
 
 	public void start(Stage primaryStage) {
-		Label label = new Label("Hello, JavaFX!");
+		Label label = new Label("Hello, Jacob!");
 		StackPane root = new StackPane(label);
 		Scene scene = new Scene(root, 300, 200);
 
